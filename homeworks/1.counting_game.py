a9fd3dd6085c2a985a01bc5bdaadcbac6d036b18
rRuseva <ruseva.radina@gmail.com>
2021-10-30 21:39:07 +0300

    first homework finished

--- homeworks/1.counting_game.py
+++ homeworks/1.counting_game.py
@@ -1,21 +1,17 @@
 """
-Имаме n човека наредени в кръг с номера от 1 до n, които участват в игра на броене наречена броенка. Играта е със следните правила:
-
-    Започваме да броим от човека с номер 1.
-
-    Отброяваме m човека участващи в кръга. Последният отброен човек (с номер m) излиза от кръга.
-
-    Повтаряме стъпка 2 (продължавайки да броим от следващия участник), докато в кръга остане само един участник. Нека номерът на участника да бъде p.
-
+Имаме n човека наредени в кръг с номера от 1 до n, които участват в игра на
+броене наречена броенка. Играта е със следните правила: Започваме да броим от
+човека с номер 1. Отброяваме m човека участващи в кръга. Последният отброен
+човек (с номер m) излиза от кръга. Повтаряме стъпка 2 (продължавайки да броим
+от следващия участник), докато в кръга остане само един участник. Нека
+номерът на участника да бъде p.
 Създайте нова функция с име find_winner:
-
 find_winner(n, m)
-
 която по подадени като аргументи n и m връща p.
-
 Напишете и код на езика Python, които да въвежда от клавиатурата n и m и да отпечатва p на екрана. 
 
-Примерни данни find_winner( 8, 3 ) --> 7, find_winner( 11, 5 ) --> 8.
+Примерни данни find_winner( 8, 3 ) --> 7, find_winner( 11, 5 ) --> 8, 
+(10, 4) --> 5,  (6, 8)  --> 3
 """
 
 
@@ -26,22 +22,22 @@ def remove_by_index(l, idx):
 
 
 def find_winer(n, m):
+	""" Iterates throug a list with numbers from 0 to n anr removes every m-th element.
+	The program ends when only one element is left. This element is returned; """
 	players = list(range(1, n+1))
-	print(players)
+	# print(players)
 	curr = m % len(players) - 1 
 	players.pop(curr)
 
 	while(len(players)>1):
-		# print(players)
-		# print("curr1: ", curr)
 		curr += (m-1)
 		if curr >= (len(players)):
 			curr = curr % len(players)
 
-		# curr -= 1
-		# print("curr2: ", curr)
-		p = players.pop(curr)
-		print("p: ", p)
+		# p = players.pop(curr)
+		p = players[curr]
+		players = remove_by_index(players, curr)
+		# print("p: ", p)
 
 	return players[0]
 
@@ -50,10 +46,7 @@ if __name__ ==  "__main__":
 	n = int(input("Please, enter people count: "))
 	m = int(input("Please, enter the counting number: "))
 
-	# (n, m) = (10, 4)
-	# (n, m) = (6, 8)
-	# (n, m) = (8, 3)
-	# (n, m) = (11, 5)
+	
 	p = find_winer(n,m)
-	print("The winner is: ", p)
-	print() 
+	print("\nThe winner is: ", p)
+	
