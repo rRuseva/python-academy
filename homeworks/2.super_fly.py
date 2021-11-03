4ab17d8b88b4a70d7b34d15b0ece7755f767bb65
rRuseva <ruseva.radina@gmail.com>
2021-11-03 22:54:17 +0200

    Value checks and string formating added

--- homeworks/2.super_fly.py
+++ homeworks/2.super_fly.py
@@ -10,15 +10,17 @@
 2) общото разстояние, което ще прелети мухата.
 """
 
-def delta_time(d, v, v2):
-    return d/(v+v2)
+def delta_time(d, v, v1):
+    """Calculates the time that the fly needs to meet the train"""
+    return d/(v+v1)
 
 def path(v, t):
     return v * t
 
 def distances(d, v, v1, v2, e):
+
     if v < v1 or v < v2:
-        return -1
+        return [], []
 
     distances = []
     dtimes = []
@@ -26,7 +28,7 @@ def distances(d, v, v1, v2, e):
     dtimes.append(dt)
     sf = path(v,dt)
     i = 1
-    while (d) >= e:
+    while (d) > e:
         distances.append(sf)
         d = d - path(v2, dt) - path(v1, dt)
         if i%2 == 0:
@@ -39,34 +41,52 @@ def distances(d, v, v1, v2, e):
 
     return distances, dtimes
 
+def print_result(distances):
+    if distances:
+        total_distance = 0
+        header = "{:>3}: {:^26}".format("#", "Distances")
+        print("\n" + header)
+        for i in range(len(distances)):
+            dist = distances[i]
+            total_distance += dist
+            print("{:>3}: {:>26.23f}".format(i, dist))
+        l = len(header)
+        total_distance_str ="Total distance: {}".format(total_distance)
+        offset = (l - len(total_distance_str))
+        print(" "*offset + total_distance_str )
+    else:
+        print("The fly is too small!")
+
+
+def print_result1(distances, times):
+    if distances and fly_times:
+        total_distance = 0
+        header = "{:>3}: {:^27} - {:^27}".format("#", "Distances", "Delta time")
+        print("\n" + header)
+        for i in range(len(distances)):
+            dist = distances[i]
+            time = times[i]
+            total_distance += dist
+            print("{:>3}: {:>27.23f} - {:>27.23f}".format(i, dist, time))
+        l = len(header)
+        total_distance_str ="Total distance: {}".format(total_distance)
+        offset = (l - len(total_distance_str))
+        print(" "*offset + total_distance_str )
+    else:
+        print("The fly is too small!")
 
 
 if __name__ == '__main__':
 
-    # d = int(input("d: "))
-    # v = int(input("v: "))
-    # v1 = int(input("v1: "))
-    # v2 = int(input("v2: "))
-    # e = int(input("e: "))
+    d = float(input("Distance between city A and city B (km):\nd =  "))
+    v = float(input("Super Fly speed (km\h):\nv = "))
+    v1 = float(input("Train A speed  (km\h):\nv1 = "))
+    v2 = float(input("Train B speed  (km\h):\nv2 = "))
+    e = float(input("The size of the super fly\ne = "))
 
-    d = 100
-    v = 30
-    v1 = 10
-    v2 = 20
-    e = 0.000001
 
     fly_distances, fly_times = distances(d, v, v1, v2, e)
+    print_result(fly_distances)
+    print()
+    # print_result1(fly_distances,fly_times)
 
-    total_distance = 0
-    header = "{:>3}: {:^26} - {:^26}".format("#", "distance", "delta time")
-    print(header)
-    # for i, dist, time in enumerate(fly_distances):
-    for i in range(len(fly_distances)):
-        dist = fly_distances[i]
-        time = fly_times[i]
-        total_distance += dist
-        print("{:>3}: {:>26.23f} - {:>26.23f}".format(i, dist, time))
-    l = len(header)
-    total_distance_str ="Total distance: {}".format(total_distance)
-    offset = (l - len(total_distance_str))
-    print(" "*offset + total_distance_str )
